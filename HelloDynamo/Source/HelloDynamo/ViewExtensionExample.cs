47b097671b953dcf6eb3b22239fd77589a697c66
Radu Gidei <radugidei@gmail.com>
2018-11-02 16:52:27 +0000

    add custom menu & menu item to view extension

--- HelloDynamo/Source/HelloDynamo/ViewExtensionExample.cs
+++ HelloDynamo/Source/HelloDynamo/ViewExtensionExample.cs
@@ -1,12 +1,8 @@
-﻿using Dynamo.Controls;
-using Dynamo.ViewModels;
+﻿using Dynamo.ViewModels;
 using Dynamo.Wpf.Extensions;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace HelloDynamo
 {
@@ -16,8 +12,11 @@ namespace HelloDynamo
     public class ViewExtensionExample : IViewExtension
     {
         public string UniqueId => "5E85F38F-0A19-4F24-9E18-96845764780C";
-
         public string Name => "Hello Dynamo View Extension";
+
+        private MenuItem extensionMenu;
+        private ViewLoadedParams viewLoadedParams;
+        private DynamoViewModel dynamoViewModel => viewLoadedParams.DynamoWindow.DataContext as DynamoViewModel;
 
         /// <summary>
         /// Method that is called when Dynamo starts, but is not yet ready to be used.
@@ -35,6 +34,32 @@ namespace HelloDynamo
         public void Loaded(ViewLoadedParams vlp)
         {
             MessageBox.Show("Hello there, viewExtension has loaded!");
+
+            // hold a reference to the Dynamo params to be used later
+            viewLoadedParams = vlp;
+
+            // we can now add custom menu items to Dynamo's UI
+            MakeMenuItems();
+        }
+
+        /// <summary>
+        /// Adds custom menu items to the Dynamo menu
+        /// </summary>
+        public void MakeMenuItems()
+        {
+            // let's now create a completely top-level new menu item
+            extensionMenu = new MenuItem { Header = "AU Workshop" };
+
+            // and now we add a new sub-menu item that says hello when clicked
+            var sayHelloMenuItem = new MenuItem { Header = "Say Hello" };
+            sayHelloMenuItem.Click += (sender, args) =>
+            {
+                MessageBox.Show("Hello " + Environment.UserName);
+            };
+            extensionMenu.Items.Add(sayHelloMenuItem);
+
+            // finally, we need to add our menu to Dynamo
+            viewLoadedParams.dynamoMenu.Items.Add(extensionMenu);
         }
 
         /// <summary>
