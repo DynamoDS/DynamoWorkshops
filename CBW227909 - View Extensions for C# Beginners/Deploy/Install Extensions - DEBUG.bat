7d2fe7912c55c877d534a5c6c775de725b706c61
Andreas Dieckmann <adieckmann@gmp-architekten.de>
2018-11-03 22:23:11 +0100

    Unfancify: Cleanup

--- CBW227909 - View Extensions for C# Beginners/Deploy/Install Extensions - DEBUG.bat
+++
@@ -1,9 +0,0 @@
-del /q "C:\Users\erudisaile\source\repos\Dynamo\bin\AnyCPU\Debug\viewExtensions\HelloDynamo_ViewExtensionDefinition.xml"
-del /q "C:\Users\erudisaile\source\repos\Dynamo\bin\AnyCPU\Debug\viewExtensions\HelloDynamo.*"
-
-xcopy /y /q "%~dp0HelloDynamo_ViewExtensionDefinition.xml" "C:\Users\erudisaile\source\repos\Dynamo\bin\AnyCPU\Debug\viewExtensions"
-
-xcopy /y /q "%~dp0HelloDynamo.dll" "C:\Users\erudisaile\source\repos\Dynamo\bin\AnyCPU\Debug\viewExtensions"
-
-pause
-exit
--- CBW227909 - View Extensions for C# Beginners/Deploy/Install Extensions.bat
+++
@@ -1,9 +0,0 @@
-del /q "C:\Program Files\Dynamo\Dynamo Core\2\viewExtensions\Unfancify_ViewExtensionDefinition.xml"
-del /q "C:\Program Files\Dynamo\Dynamo Core\2\viewExtensions\Unfancify.*"
-
-xcopy /y /q "%~dp0Unfancify_ViewExtensionDefinition.xml" "C:\Program Files\Dynamo\Dynamo Core\2\viewExtensions"
-
-xcopy /y /q "%~dp0Unfancify.dll" "C:\Program Files\Dynamo\Dynamo Core\2\viewExtensions"
-
-pause
-exit
