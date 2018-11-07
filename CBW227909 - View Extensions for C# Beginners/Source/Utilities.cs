84187df32682b7ea2bc674bef700dfa1bbd6bb7d
Andreas Dieckmann <adieckmann@gmp-architekten.de>
2018-11-07 20:56:13 +0100

    Unfancify: Reformatting based on new editorconfig

--- CBW227909 - View Extensions for C# Beginners/Source/Utilities.cs
+++ CBW227909 - View Extensions for C# Beginners/Source/Utilities.cs
@@ -3,28 +3,28 @@ using System.Reflection;
 
 namespace Unfancify
 {
+  /// <summary>
+  /// Shared general utility functions
+  /// </summary>
+  static class GeneralUtils
+  {
     /// <summary>
-    /// Shared general utility functions
+    /// Dynamo's public API does not offer a method to clear the current selection.
+    /// However, Dynamo does contain an internal DynamoSelection class that includes such a method:
+    /// https://github.com/DynamoDS/Dynamo/blob/RC2.0.1_master/src/DynamoCore/Core/DynamoSelection.cs#L94
+    /// This method is exposed here via reflection.
+    /// More on reflection here: https://stackify.com/what-is-c-reflection/
     /// </summary>
-    static class GeneralUtils
+    public static void ClearSelection()
     {
-        /// <summary>
-        /// Dynamo's public API does not offer a method to clear the current selection.
-        /// However, Dynamo does contain an internal DynamoSelection class that includes such a method:
-        /// https://github.com/DynamoDS/Dynamo/blob/RC2.0.1_master/src/DynamoCore/Core/DynamoSelection.cs#L94
-        /// This method is exposed here via reflection.
-        /// More on reflection here: https://stackify.com/what-is-c-reflection/
-        /// </summary>
-        public static void ClearSelection()
-        {
-            // Find the DynamoSelection class
-            var dynamoSelection = typeof(DynamoModel).Assembly.GetType("Dynamo.Selection.DynamoSelection");
-            // Instantiate the class
-            var selectionInstance = dynamoSelection.GetProperty("Instance", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static);
-            // Find the ClearSelection method
-            var clearMethod = dynamoSelection.GetMethod("ClearSelection", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
-            // Call the ClearSelection method
-            clearMethod.Invoke(selectionInstance.GetValue(null), null);
-        }
+      // Find the DynamoSelection class
+      var dynamoSelection = typeof(DynamoModel).Assembly.GetType("Dynamo.Selection.DynamoSelection");
+      // Instantiate the class
+      var selectionInstance = dynamoSelection.GetProperty("Instance", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static);
+      // Find the ClearSelection method
+      var clearMethod = dynamoSelection.GetMethod("ClearSelection", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+      // Call the ClearSelection method
+      clearMethod.Invoke(selectionInstance.GetValue(null), null);
     }
+  }
 }
