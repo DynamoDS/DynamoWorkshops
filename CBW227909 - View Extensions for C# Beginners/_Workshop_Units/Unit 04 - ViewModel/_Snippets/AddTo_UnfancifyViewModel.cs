bf99eb02f860b65bdbed666f4a0c27b9824a0a55
Andreas Dieckmann <adieckmann@gmp-architekten.de>
2018-11-08 21:04:08 +0100

    Unfancify Unit 04

---
+++ CBW227909 - View Extensions for C# Beginners/_Workshop_Units/Unit 04 - ViewModel/_Snippets/AddTo_UnfancifyViewModel.cs
@@ -0,0 +1,28 @@
+using System.Windows.Input;
+using Dynamo.UI.Commands;
+
+
+
+    public ICommand UnfancifyCurrentGraph { get; set; }
+
+
+
+      // The Unfancify Current Graph button is bound to this command
+      UnfancifyCurrentGraph = new DelegateCommand(OnUnfancifyCurrentClicked);
+
+
+
+    /// <summary>
+    /// Text message that appears below the buttons.
+    /// It is updated by some of the methods in this view model.
+    /// </summary>
+    public string UnfancifyMsg
+    {
+      get { return unfancifyMsg; }
+      set
+      {
+        unfancifyMsg = value;
+        // Notify the UI that the value has changed
+        RaisePropertyChanged("UnfancifyMsg");
+      }
+    }
