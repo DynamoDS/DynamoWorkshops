5179603fbb816fc6d41cd3120440f2bbc03d3d81
Andreas Dieckmann <adieckmann@gmp-architekten.de>
2018-11-08 15:26:15 +0100

    Unfancify Unit 07

---
+++ CBW227909 - View Extensions for C# Beginners/_Workshop_Units/Unit 07 - Modify Node Values/Source/UnfancifyWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+
+namespace Unfancify
+{
+  /// <summary>
+  /// Interaction logic for UnfancifyWindow.xaml
+  /// </summary>
+  public partial class UnfancifyWindow : Window
+  {
+
+    /// <summary>
+    /// The form constructor.
+    /// </summary>
+    public UnfancifyWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
