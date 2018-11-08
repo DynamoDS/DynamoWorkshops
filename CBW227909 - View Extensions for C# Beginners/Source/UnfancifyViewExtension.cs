8b6d9f33f92be8067c1cfdd34e6d06602e2ee50d
Andreas Dieckmann <adieckmann@gmp-architekten.de>
2018-11-08 21:46:34 +0100

    Reformat in final version too

--- CBW227909 - View Extensions for C# Beginners/Source/UnfancifyViewExtension.cs
+++ CBW227909 - View Extensions for C# Beginners/Source/UnfancifyViewExtension.cs
@@ -54,21 +54,21 @@ namespace Unfancify
       // Define what happens when our sub-menu item is clicked
       unfancifyMenuItem.Click += (sender, args) =>
       {
-              // Instantiate the view model of our tool
-              var viewModel = new UnfancifyViewModel(viewLoadedParams, dynamoViewModel, viewLoadedParams.DynamoWindow);
-              // Create the window for our UI
-              var window = new UnfancifyWindow
+        // Instantiate the view model of our tool
+        var viewModel = new UnfancifyViewModel(viewLoadedParams, dynamoViewModel, viewLoadedParams.DynamoWindow);
+        // Create the window for our UI
+        var window = new UnfancifyWindow
         {
-                // Set our view model as the DataContext of the main panel of our UI
-                unfancifyPanel = { DataContext = viewModel },
-                // Set our window as a child of the Dynamo window
-                Owner = viewLoadedParams.DynamoWindow
+          // Set our view model as the DataContext of the main panel of our UI
+          unfancifyPanel = { DataContext = viewModel },
+          // Set our window as a child of the Dynamo window
+          Owner = viewLoadedParams.DynamoWindow
         };
-              // Define the start position of our window
-              window.Left = window.Owner.Left + 400;
+        // Define the start position of our window
+        window.Left = window.Owner.Left + 400;
         window.Top = window.Owner.Top + 200;
-              // Display our window
-              window.Show();
+        // Display our window
+        window.Show();
       };
       // Add our sub-menu item to our top-level menu item
       extensionMenu.Items.Add(unfancifyMenuItem);
