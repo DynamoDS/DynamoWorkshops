149bcb27615a994c64cd4596b5401324fb9a913e
Matteo Cominetti <matteo@cominetti.org>
2018-11-09 20:20:53 +0000

    my stuff

---
+++ CBW227912 - Leveraging Speckle in Dynamo/1 - HelloDynamo - end/HelloDynamo/NodeTracker.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace HelloDynamo
+{
+  /// <summary>
+  /// Tracks and displays events for node/connector added/removed 
+  /// </summary>
+  public partial class NodeTracker : Window
+  {
+    public NodeTracker()
+    {
+      InitializeComponent();
+      this.Closing += NodeTracker_Closing;
+    }
+
+    private void NodeTracker_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+      //hide window instead of closing it, so we keep tracking in the background
+      e.Cancel = true;
+      this.Hide();
+    }
+  }
+}
