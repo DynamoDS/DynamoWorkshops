899af5dd063f24b694cccf5cc782c80530b12f2b
Radu Gidei <radugidei@gmail.com>
2019-04-03 20:44:28 +0100

    add exercise 8-UI

---
+++ CBW227911 - Control Dynamo from the Web/Exercises/HelloDynamo/8-Hello-UI/HelloDynamo/Stats/StatsViewModel.cs
@@ -0,0 +1,79 @@
+﻿using Dynamo.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dynamo.Extensions;
+using Dynamo.Graph.Nodes;
+using Dynamo.ViewModels;
+using System.Windows;
+using Dynamo.Graph;
+using Dynamo.Wpf.Extensions;
+using Dynamo.Graph.Annotations;
+using Dynamo.Graph.Workspaces;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HelloDynamo.Stats
+{
+    class StatsViewModel : NotificationObject
+    {
+        private ViewLoadedParams readyParams;
+        private string activeNodeCount;
+        private string activeWireCount;
+
+        //Displays the count of the nodes in the workspace
+        public string ActiveNodeCount => readyParams.CurrentWorkspaceModel.Nodes.Count().ToString();
+
+        //Displays the count of the wires in the workspace
+        public string ActiveWireCount => readyParams.CurrentWorkspaceModel.Connectors.Count().ToString();
+
+        public StatsViewModel(ReadyParams p)
+        {
+            readyParams = p as ViewLoadedParams;
+            readyParams.CurrentWorkspaceModel.NodeAdded += OnNodeCountChange;
+            readyParams.CurrentWorkspaceModel.NodeRemoved += OnNodeCountChange;
+            readyParams.CurrentWorkspaceModel.ConnectorAdded += OnWireCountChange;
+            readyParams.CurrentWorkspaceModel.ConnectorDeleted += OnWireCountChange;
+            readyParams.CurrentWorkspaceModel.NodeAdded += OnNodesUpdate;
+            readyParams.CurrentWorkspaceModel.NodeRemoved += OnNodesUpdate;
+            readyParams.CurrentWorkspaceChanged += OnWorkspaceChange;
+        }
+
+        #region ChangeProperty events
+
+        private void OnNodeCountChange(NodeModel obj)
+        {
+            RaisePropertyChanged("ActiveNodeCount");
+        }
+
+        private void OnWireCountChange(Dynamo.Graph.Connectors.ConnectorModel obj)
+        {
+            RaisePropertyChanged("ActiveWireCount");
+        }
+
+        private void OnNodesUpdate(NodeModel obj)
+        {
+            RaisePropertyChanged("ActiveNodeCount");
+        }
+
+        private void OnWorkspaceChange(Dynamo.Graph.Workspaces.IWorkspaceModel obj)
+        {
+            RaisePropertyChanged("ActiveNodeCount", "ActiveWireCount");
+        }
+
+        #endregion
+
+        public void Dispose()
+        {
+            readyParams.CurrentWorkspaceModel.NodeAdded -= OnNodeCountChange;
+            readyParams.CurrentWorkspaceModel.NodeRemoved -= OnNodeCountChange;
+            readyParams.CurrentWorkspaceModel.ConnectorAdded += OnWireCountChange;
+            readyParams.CurrentWorkspaceModel.ConnectorDeleted += OnWireCountChange;
+            readyParams.CurrentWorkspaceModel.NodeAdded += OnNodesUpdate;
+            readyParams.CurrentWorkspaceModel.NodeRemoved += OnNodesUpdate;
+            readyParams.CurrentWorkspaceChanged += OnWorkspaceChange;
+        }
+    }
+}
