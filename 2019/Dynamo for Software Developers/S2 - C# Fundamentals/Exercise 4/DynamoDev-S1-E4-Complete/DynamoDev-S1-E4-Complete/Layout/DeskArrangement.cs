6f39d69deb31bc57172e93e17d9c95a036a5d2a8
Radu Gidei <radumg@users.noreply.github.com>
2019-11-15 16:21:27 +0000

    Add S2E4

---
+++ 2019/Dynamo for Software Developers/S2 - C# Fundamentals/Exercise 4/DynamoDev-S1-E4-Complete/DynamoDev-S1-E4-Complete/Layout/DeskArrangement.cs
@@ -0,0 +1,63 @@
+﻿using Autodesk.DesignScript.Geometry;
+using DynamoDev.Business;
+using System;
+using System.Collections.Generic;
+
+namespace DynamoDev.Layout
+{
+    public class DeskArrangement : Identifiable
+    {
+        private const double DEFAULT_EFFICIENCY_TARGET = 0.8;
+        private readonly double sourceSurfaceArea;
+
+        /// <summary>
+        /// The desks included in the arrangement.
+        /// </summary>
+        public List<Desk> Desks { get; }
+        public DateTime CreatedAt { get; }
+        /// <summary>
+        /// The number of desks in the arrangement.
+        /// </summary>
+        public int DeskCount => this.Desks is null ? this.Desks.Count : 0;
+
+        /// <summary>
+        /// Returns the actual yield of the desk arrangement, 
+        /// expressed as the number of desks divided by the surface area they are meant to occupy.
+        /// </summary>
+        public double ActualYield => CalculateYield();
+
+        /// <summary>
+        /// The target yield, as a percentage.
+        /// </summary>
+        public double TargetYield { get; set; }
+
+        /// <summary>
+        /// Expresses the yield efficiency of actual yield divided by target yield, as a percentage.
+        /// </summary>
+        public double Efficiency => CalculateEfficiency();
+
+        public double Revenue { get; set; }
+
+        public DeskArrangement(List<Desk> desks, Surface surface) : base()
+        {
+            if (surface is null)
+                throw new ArgumentNullException(nameof(surface));
+
+            this.Desks = desks ?? throw new ArgumentNullException(nameof(desks));
+
+            this.sourceSurfaceArea = surface.Area;
+            this.TargetYield = DEFAULT_EFFICIENCY_TARGET * this.sourceSurfaceArea;
+        }
+
+        private double CalculateYield()
+        {
+            return this.DeskCount / this.sourceSurfaceArea;
+        }
+
+        private double CalculateEfficiency()
+        {
+            if (this.TargetYield == 0) throw new DivideByZeroException("Target yield cannot be 0.");
+            return this.ActualYield / this.TargetYield * 100;
+        }
+    }
+}
